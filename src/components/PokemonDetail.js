85726467a12395b4751792c5b2f199cb9f601747
Alejandro Garcia <alejandrojg8@gmail.com>
2022-08-20 21:54:46 +0100

    css progress

--- src/components/PokemonDetail.js
+++ src/components/PokemonDetail.js
@@ -1,15 +1,38 @@
 import React from "react";
-import './PokemonDetail.js'
+import './card.css'
 
 
-const PokemonDetail = ({pokemon}) =>{
+const PokemonDetail = ({ pokemon }) => {
 
 
 
-    return(
-<div className="pokemon">
-    <h2>{pokemon.name}</h2>
-</div>
+    return (
+        <div className="Card">
+
+            <div className="poke__img">
+                <img src={pokemon.sprites.front_default} />
+            </div>
+
+            <div className="poke_name">
+                <h2>{pokemon.name}</h2>
+            </div>
+
+            <div className="poke_type">
+                {   
+                    pokemon.types.map(type => {
+                        return (
+                            <div className="Card_type">
+                                {type.type.name}
+                            </div>
+                        )
+                    })
+
+                }
+            </div>
+
+
+        </div>
+
     )
 }
 
--- src/containers/PokemonContainer.js
+++ src/containers/PokemonContainer.js
@@ -2,52 +2,72 @@ import React, { useEffect, useState } from "react";
 import PokemonDetail from "../components/PokemonDetail";
 import PokemonSelector from "../components/PokemonSelector";
 import './PokemonContainer.css'
+import { getAllPokemons } from "../components/Services/fetchPokemon";
+import { getPokemon } from "../components/Services/fetchPokemon";
 
 
 const PokemonContainer = () => {
 
-    const [pokemonList, setPokemonList] = useState([]);
+    const [pokemonData, setPokemonData] = useState([]);
     const [pokemonSelected, setPokemonSelected] = useState(null);
     const [loading, setLoading] = useState(true)
     const initialUrl = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=151"
+    const navImage = "https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png"
+    const pokedexImage = "https://www.kindpng.com/picc/m/541-5418191_original-pokedex-hd-png-download.png"
+
 
     useEffect(() => {
-        getPokemons();
+        async function fecthData() {
+            let response = await getAllPokemons(initialUrl);
+            await loadingPokemon(response.results);
+            setLoading(false)
+        };
+        fecthData()
     }, [])
 
-    const onPokemonSelected = (pokemon) =>{
-setPokemonSelected(pokemon);
+    const onPokemonSelected = (pokemon) => {
+        setPokemonSelected(pokemon);
     }
-    
+
+    const loadingPokemon = async (data) => {
+        let _pokemon = await Promise.all(data.map(async pokemon => {
+            let pokemonRecord = await getPokemon(pokemon.url);
+            return pokemonRecord
+
+        }))
+
+        setPokemonData(_pokemon)
+    }
 
 
-    const getPokemons = function () {
-        fetch(initialUrl)
-            .then(res => res.json())
-            .then(poke => setPokemonList(poke.results))
-    }
-    const imageUrl = "https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png"
+
+
 
 
 
     return (
-<>
-<div className="img">
-<img src={imageUrl} alt="pokeapi-logo" className="image"></img>
-</div>
+        <>
 
-        <nav className="nav">
-        
+            <div className="img">
+                <img src={navImage} alt="pokeapi-logo" className="image"></img>
+            </div>
 
-        
+            <nav className="nav">
+                <PokemonSelector pokemons={pokemonData} onPokemonSelected={onPokemonSelected} />
+            </nav>
 
-            <PokemonSelector pokemons = {pokemonList} onPokemonSelected ={onPokemonSelected}/>
-            {pokemonSelected ? <PokemonDetail pokemon={pokemonSelected}/> : null}
+            <div className="grid-container">
+                {pokemonSelected ? <PokemonDetail pokemon={pokemonSelected} /> : <img src={pokedexImage} alt="pokedex" className="pokedex"></img>
+}
+                
+
+            </div>
 
 
 
-        </nav>
-</>
+
+
+        </>
     )
 }
 
